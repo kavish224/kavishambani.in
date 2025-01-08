97b33acce577ba86021e7c84a01cd0960659a010
Kavish Ambani <kavishkkambani@gmail.com>
2025-01-09 00:52:56 +0530

    add metadatabase

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
+  metadataBase: new URL('https://kavishambani.in'),
   title: 'Kavish Ambani - Personal Website',
   description: 'Official website of Kavish Ambani.',
   openGraph: {
