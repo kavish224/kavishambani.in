bf92fe47d47273254c79d51d6989a10e14db908c
Kavish Ambani <kavishkkambani@gmail.com>
2025-01-09 00:43:41 +0530

    first commit

---
+++ next-sitemap.config.ts
@@ -0,0 +1,6 @@
+module.exports = {
+    siteUrl: 'https://kavishambani.in',
+    generateRobotsTxt: true, // Automatically generate a robots.txt
+    sitemapSize: 7000, // Adjust as needed
+  };
+  
