a21d1325912ebe52a1f5853b175892d979401f35
Kavish Ambani <kavishkkambani@gmail.com>
2025-01-09 10:25:16 +0530

    setup sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://kavishambani.in', // Replace with your domain
+    generateRobotsTxt: true, // Generates robots.txt alongside sitemap
+  };
+  
--- next-sitemap.config.ts
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    siteUrl: 'https://kavishambani.in',
-    generateRobotsTxt: true, // Automatically generate a robots.txt
-    sitemapSize: 7000, // Adjust as needed
-  };
-  
